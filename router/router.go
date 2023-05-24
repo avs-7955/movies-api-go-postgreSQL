3583199a8dcc3ec051911a0699fb25ab57dc0408
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-24 10:29:01 +0530

    routers

--- router/router.go
+++ router/router.go
@@ -1,1 +1,18 @@
 package router
+
+import (
+	controller "movies-api-go-post/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/movies", controller.GetMovies).Methods("GET")
+	r.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	r.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
+	r.HandleFunc("/api/movie/{id}", controller.DeleteMovie).Methods("DELETE")
+	r.HandleFunc("/api/del", controller.DeleteMovies).Methods("DELETE")
+
+	return r
+}
