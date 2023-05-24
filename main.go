36cc5eef595aff126c75a89a9483985b193bb04b
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-24 10:30:20 +0530

    main function

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"movies-api-go-post/router"
+	"net/http"
 
 	_ "github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -9,4 +12,9 @@ import (
 
 func main() {
 	fmt.Println("PostgreSQL Movies API")
+	r := router.Router()
+	fmt.Println("Server is getting started....")
+	// listening to a port
+	log.Fatal(http.ListenAndServe(":3000", r))
+	fmt.Println("Listening at port 3000....")
 }
